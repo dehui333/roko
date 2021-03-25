58302753315c4c53a0c200795d8a50e15efdefb0
tbrekalo <brekalo.tvrtko@gmail.com>
2021-03-25 13:13:27 +0000

    cleared up dependencies

--- setup.py
+++ setup.py
@@ -1,30 +1,60 @@
 #!/usr/bin/env python3
 # encoding: utf-8
 
-from distutils.core import setup, Extension
+
+import subprocess
+import platform
+import pathlib
+import os
 
 
-def configuration(parent_package='', top_path=None):
-      import numpy
-      from numpy.distutils.misc_util import Configuration
-      from numpy.distutils.misc_util import get_info
+from setuptools import Extension, setup
+from setuptools.command.build_ext import build_ext
 
-      config = Configuration('',
-                             parent_package,
-                             top_path)
-      config.add_extension('gen',
-                           ['gen.cpp', 'generate.cpp', 'models.cpp'],
-                           extra_objects=['Dependencies/htslib-1.9/libhts.a'],
-                           extra_compile_args=['-std=c++14'], language='c++',
-                           extra_link_args=['-lz', '-llzma', '-lbz2', '-lz', '-lm', '-lpthread', '-lcurl', '-lcrypto'],
-                           include_dirs=['Dependencies/htslib-1.9/htslib', 'include'])
 
-      return config
+class CMakeExtension(Extension):
+  def __init__(self, name):
+        super().__init__(name=name, sources=[])
+
+
+class CMakeBuild(build_ext):
+  def run(self):
+    self.__check_cmake()
+    for extension in self.extensions:   
+      if isinstance(extesnion, CMakeExtension):
+        self.__run_cmake(extension)              
+
+  @staticmethod
+  def __check_cmake():
+    try:        
+      subprocess.check_call(['which', 'cmake'])
+      return True
+    except subprocess.CalledProcessError:                
+      raise RuntimeError('CMake not found')
+
+
+  def __run_cmake(self, extension):
+    # TODO: account for modularity
+    # make -H./ extension dependent
+    build_dir = pathlib.Path(self.build_temp)
+    os.makedirs(build_dir, exist_ok=True)
+
+    cmake_args = [
+      '-H./',
+      '-B{}'.format(build_dir),
+      '-DCMAKE_EXPORT_COMPILE_COMMANDS=1'
+    ]
+
+    self.spawn(['cmake'] + cmake_args)
+    self.spawn(['cmake', '--build', build_dir])
 
 if __name__ == "__main__":
-      from numpy.distutils.core import setup
-
-      setup(
-        name='gen',
-        version='0.0.1',
-        configuration=configuration)
+  if platform.system() == 'Windows':
+    raise RuntimeError('Unsupported platform.')
+        
+  setup(
+    name='roko',
+    version='0.1.0',
+    ext_modules=[CMakeExtension('_roko')],
+    cmdclass={'build_ext': CMakeBuild}
+  )
